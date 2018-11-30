e62ae5ed2e34fd43f2744e5404d44bab496278b0
123kieran <123kieran@gmail.com>
2018-11-30 17:13:57 +0000

    imports added and read in files

--- digit-recognition-script/digitrec.py
+++ digit-recognition-script/digitrec.py
@@ -1,1 +1,20 @@
-print("Hello, World!")
+# Imports
+import keras as kr
+import numpy as np
+import sklearn.preprocessing as pre
+import gzip
+
+
+
+# Read in all the files
+with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
+    test_images = f.read()
+    
+with gzip.open('data/t10k-labels-idx1-ubyte.gz', 'rb') as f:
+    test_labels = f.read()
+    
+with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
+    training_images = f.read()
+    
+with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:
+    training_labels = f.read()
